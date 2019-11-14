c219f3e4fd728f362e842ad5f332394c3bc7ed26
Guillaume <guillaume.pelletier@reseau.eseo.fr>
2019-11-14 10:19:09 +0100

    Add : onResume, onStop, onPause, onDestroy, onCreate : GP

--- app/src/main/java/com/example/ma_liste_de_course/MainActivity.java
+++ app/src/main/java/com/example/ma_liste_de_course/MainActivity.java
@@ -6,9 +6,49 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private static final String TAG = "Liste de courses";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        @Override
+        protected void onStart() {
+            super.onStart();
+            Log.i(TAG, "on start" + getLocalClassName());
+        }
+
+
+        @Override
+        protected void onStop() {
+            super.onStop();
+            Log.i(TAG, "on start" + getLocalClassName());
+        }
+
+
+        @Override
+        protected void onPause() {
+            super.onPause();
+            Log.i(TAG, "on Pause" + getLocalClassName());
+        }
+
+
+        @Override
+        protected void onDestroy() {
+            super.onDestroy();
+            Log.i(TAG, "on Destroy" + getLocalClassName());
+        }
+
+        @Override
+        protected void onResume() {
+            super.onResume();
+            Log.i(TAG, "on Pause" + getLocalClassName());
+        }
+
+
     }
 }
